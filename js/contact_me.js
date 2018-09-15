59ebb575eef7687ed193779ba4b0fab4fd34e205
dhan78 <emugundhan@gmail.com>
2018-09-15 12:14:54 -0400

    adding changes

--- js/contact_me.js
+++ js/contact_me.js
@@ -29,24 +29,25 @@ $(function() {
           message: message
         },
         cache: false,
-        success: function() {
+        success: function(response) {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Your message has been sent. We will reach out to you shortly.</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        error: function() {
+        error: function(response) {
           // Fail message
+	console.log(response)
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not configured yet..working on it. Please try again later!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
